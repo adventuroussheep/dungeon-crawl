7ed6cab7de366af84db94d3331d26829c348d626
Jacob Haas <jacob_haas_7@hotmail.com>
2019-10-29 12:07:23 -0700

    color scheme

--- src/Components/Homepage/Home.js
+++ src/Components/Homepage/Home.js
@@ -3,16 +3,34 @@ import React from 'react';
 import {getNames, namesList} from "../utils/api";
 import Fab from '@material-ui/core/Fab';
 import Button from "@material-ui/core/Button"
+// import {red, yellow} from "@material-ui/core/colors";
+// import { createMuiTheme } from '@material-ui/core';
+
+
+
 import "./home.css";
+
+
+
 
 // var api = require('../Components/utils/api');
 // var nameArray = [];
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: red,
+//     secondary: {
+//       main: yellow[500],
+//       // light: yellow[700],
+//     }
+//   }
+// })
 
 
 class HomePage extends React.Component{
   constructor(props){
     super(props);
-
+    
     this.getNames = getNames.bind(this);
     // this.namesList = namesList.bind(this);
     
@@ -29,7 +47,7 @@ class HomePage extends React.Component{
     getNames().then(() => {
       namesList.map((namesList, i) => {
         // this.setState({className:[...this.state.className, <button className={i} key={namesList.i}>{namesList}</button>]});
-        this.setState({className:[...this.state.className, <Button variant="outlined" color="inherit" className={i}
+        this.setState({className:[...this.state.className, <Button variant="filled" color="secondary" className={i}
         key={namesList.i}> {namesList}
         </Button>]});
         return namesList;
@@ -38,7 +56,6 @@ class HomePage extends React.Component{
     })
   }
 
-
     render(){
       return (
         
@@ -46,7 +63,7 @@ class HomePage extends React.Component{
       <h1>err, {this.props.repos}</h1>
       <h1> {this.state.className}</h1>
       <div className="centerStartBtn">
-      <Fab onClick={this.changeText} variant="extended" color="primary" aria-label="delete" className="startButton">
+      <Fab onClick={this.changeText} variant="extended" color="secondary" aria-label="delete" className="startButton">
         Start
       </Fab>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#FFFEBD',
+      },
+      secondary: {
+        main: "#F7CE65",
+        // light: yellow[700],
+      },
+      redWine: {
+          main: "#930C10",
+      },
+      brown: {
+          main: "#290000",
+      }
+      ,
+      black: {
+          main: "#030303",
+      }
+    }
+  })
+  
+
+ReactDOM.render(
+<MuiThemeProvider theme={theme}>
+<App />
+</MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
